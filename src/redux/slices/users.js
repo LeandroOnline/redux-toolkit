5d030d91d0ac799794f71784cf14f49d06098ae4
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-01-17 20:57:57 -0300

    m

--- src/redux/slices/users.js
+++ src/redux/slices/users.js
@@ -1,12 +1,11 @@
 // slices hace referencia a "porcion del estado"
-// en redux pueden haber varios slices, uno para los usuarios, otro para los productos, es una parte
-// del estado
+// en redux pueden haber varios slices, uno para los usuarios, otro para los productos, es una parte del estado
 
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const userSlice = createSlice({
-    name: 'users',                          // nombre de esta parte del estado, es decir de este slice
+    name: 'users',      // nombre de esta parte del estado, es decir de este slice
     initialState: {
         list: []
     },
@@ -15,14 +14,14 @@ export const userSlice = createSlice({
             state.list = action.payload;    // el state guarda el valor de initialState como en redux normal
                                             // action.payload guarda todo la data que se envia con el dispatch
         },
-        setAlgo:{}
+        setAlgo: ()=> {}
     }
 })
-console.log(userSlice)
+// console.log(userSlice)
+
 
 export const {setUserList} = userSlice.actions;
 export default userSlice.reducer;
-
 
 export const fetchAllUsers= () => (dispatch) => {
     axios.get('https://reqres.in/api/users?per_page=12')
